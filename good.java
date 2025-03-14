caf5c895913e9c089fb936004433fc9ce6d97461
kikkiredygowthamreddy <gowthamreddykikki6@gmail.com>
2025-03-14 03:39:35 -1200

    Add files via upload

---
+++ good.java
@@ -0,0 +1,8 @@
+class good{
+    public static void main(String args[]){
+
+    String name = new String("ravi");
+    System.out.println("hello" + name);
+System.out.println(name.charAt(2));
+    }
+}
